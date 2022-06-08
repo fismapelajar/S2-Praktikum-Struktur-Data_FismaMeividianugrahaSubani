f4a1860d73bf248da5460a4dbe948fefc37fc650
fismapelajar <90302378+fismapelajar@users.noreply.github.com>
2022-06-08 10:20:48 +0700

    Add files via upload

---
+++ 11. UAS Struktur Data/1. Undirected Graph_017_Fisma Meividianugraha Subani.cpp
@@ -0,0 +1,39 @@
+// Program C++ implementasi undirected dan weighted graph ke bentuk adjacency list.
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+// Fungsi Void addEdge untuk menambahkan edge
+void addEdge(vector <pair<int, int> > adj[], int u, int v, int wt) {
+	adj[u].push_back(make_pair(v, wt));
+	adj[v].push_back(make_pair(u, wt));
+}
+
+// Fugsi Void printGraph untuk merubah graph menjadi adjacency list
+void printGraph(vector<pair<int,int> > adj[], int V) {
+	int v, w;
+	for (int u = 1; u < V; u++) {
+		cout << u;
+		for (auto it = adj[u].begin(); it!=adj[u].end(); it++) {
+			v = it->first;
+			w = it->second;
+			cout  << " -> " << "[" << v << "," << w << "]";
+		}
+		cout << "\n";
+	}
+}
+
+int main() {
+	int V = 5;
+	vector<pair<int, int> > adj[V];
+	// Menambahkan nilai edge
+	addEdge(adj, 1, 2, 5);
+	addEdge(adj, 2, 3, 1);
+	addEdge(adj, 4, 1, 3);
+	addEdge(adj, 2, 4, 1);
+	addEdge(adj, 3, 1, 1);
+	
+	// Mencetak graph
+	printGraph(adj, V);
+	return 0;
+}
